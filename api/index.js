f274b0f5f252129450121f1ed41830bbd06aaddd
Luka <luka.cerovic14@gmail.com>
2024-02-01 13:42:15 +0100

    Creating Servier Side

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to Mongo DB');  // .then(() => ) ..... provera da smo se stvarno konektovali sa MongoDb-em
+}).catch((err) => {  // u slucaju da nismo konektovani, da nam uhvati gresku
+    console.log(err);
+});
+
+// Creating application
+const app = express();
+
+app.use(express.json());
+
+// kreiramo rikvestove
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
