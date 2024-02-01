fc1448622af01bb72f5ae4e0b79e9d8649c0e884
Luka <luka.cerovic14@gmail.com>
2024-02-01 14:59:31 +0100

    Add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import hotelReducer from './hotel/hotelSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({ hotel: hotelReducer });
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {hotel: hotelReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   }),
-})
+});
+
+export const persistor = persistStore(store);
